fe9312086893083f7bdba4a078242419a8a6b36e
jeeshanmohammad9917 <134601978+jeeshanmohammad9917@users.noreply.github.com>
2023-08-05 20:33:56 +0530

    form-builder

--- public/admin_assets/js/index.js
+++ public/admin_assets/js/index.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function clearFormPreview() {
      
-        formbuilder.innerHTML = ''; // Clear the form preview
+        formbuilder.innerHTML = ''; 
 
     }
 
@@ -42,28 +42,28 @@ document.addEventListener('DOMContentLoaded', function() {
 
         switch (elementType) {
             case 'input-text':
-                // If the element type is 'input-text', create an <input> element with type 'text' and a placeholder.
+               
                 formElement.innerHTML = 'Name :'+'<br>'+'<input type="text" name="name" placeholder="Input Text">';
                      break;
             case 'input-email':
-                // If the element type is 'input-email', create an <input> element with type 'email' and a placeholder.
+                
                 formElement.innerHTML = 'Email :'+'<br>'+'<input type="email" name="email" placeholder="Input Email">';
                      break;
             case 'input-password':
-            // If the element type is 'input-email', create an <input> element with type 'email' and a placeholder.
+        
                 formElement.innerHTML = 'Password :'+'<br>'+'<input type="password" name="password" placeholder="Input password">';
                      break;         
             case 'input-number':
-                    // If the element type is 'input-email', create an <input> element with type 'email' and a placeholder.
+               
                 formElement.innerHTML = 'Mobile :'+'<br>'+'<input type="text" name="contact" placeholder="Input Number">';
                     break;
             case 'input-submit':
-                        // If the element type is 'input-email', create an <input> element with type 'email' and a placeholder.
+                 
                  formElement.innerHTML = '<input type="submit" name="submit" placeholder="Input Submit" >';
                     break;
-            // Add more cases for other input types, each with their respective HTML content.
+           
             default:
-                // If the element type is not recognized, display 'Unsupported Element'.
+             
                 formElement.textContent = 'Unsupported Element';
                 break;
         }
@@ -87,6 +87,6 @@ document.addEventListener('DOMContentLoaded', function() {
     const saveButton = document.getElementById('save-button');
     saveButton.addEventListener('click', saveFormElements,clearFormPreview);
 
-    const clearButton = document.getElementById('save-button'); // Add a clear button in your HTML
-    clearButton.addEventListener('click', clearFormPreview); // Attach the clearFormPreview function
+    const clearButton = document.getElementById('save-button');
+    clearButton.addEventListener('click', clearFormPreview);
 });
